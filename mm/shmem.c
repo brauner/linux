0b62325fd7c4ed55e747e6cfdd27658cc65f5080
Jeff Layton <jlayton@kernel.org>
2023-05-18 07:47:39 -0400

    tmpfs: add support for multigrain timestamps
    The fine grained time is needed for NFS.
    
    Signed-off-by: Jeff Layton <jlayton@kernel.org>
    Message-Id: <20230518114742.128950-7-jlayton@kernel.org>
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- mm/shmem.c
+++ mm/shmem.c
@@ -4085,7 +4085,7 @@ static struct file_system_type shmem_fs_type = {
 #endif
 	.kill_sb	= kill_litter_super,
 #ifdef CONFIG_SHMEM
-	.fs_flags	= FS_USERNS_MOUNT | FS_ALLOW_IDMAP,
+	.fs_flags	= FS_USERNS_MOUNT | FS_ALLOW_IDMAP | FS_MULTIGRAIN_TS,
 #else
 	.fs_flags	= FS_USERNS_MOUNT,
 #endif
