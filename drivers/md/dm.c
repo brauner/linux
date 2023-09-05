19a47c0e08823b50ce21e360c79a35b665ffd1ce
Christian Brauner <brauner@kernel.org>
2023-09-05 15:05:02 +0200

    bdev: rename freeze and thaw helpers
    We have bdev_mark_dead() etc and we're going to move block device
    freezing to holder ops in the next patch. Make the naming consistent:
    
    * freeze_bdev() -> bdev_freeze()
    * thaw_bdev()   -> bdev_thaw()
    
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- drivers/md/dm.c
+++ drivers/md/dm.c
@@ -2667,7 +2667,7 @@ static int lock_fs(struct mapped_device *md)
 
 	WARN_ON(test_bit(DMF_FROZEN, &md->flags));
 
-	r = freeze_bdev(md->disk->part0);
+	r = bdev_freeze(md->disk->part0);
 	if (!r)
 		set_bit(DMF_FROZEN, &md->flags);
 	return r;
@@ -2677,7 +2677,7 @@ static void unlock_fs(struct mapped_device *md)
 {
 	if (!test_bit(DMF_FROZEN, &md->flags))
 		return;
-	thaw_bdev(md->disk->part0);
+	bdev_thaw(md->disk->part0);
 	clear_bit(DMF_FROZEN, &md->flags);
 }
 
