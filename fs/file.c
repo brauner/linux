d57eebc7f9f3fa0956b590e8dc9d3ff03c7df087
Yu Ma <yu.ma@intel.com>
2024-07-03 10:33:09 -0400

    fs/file.c: remove sanity_check and add likely/unlikely in alloc_fd()
    alloc_fd() has a sanity check inside to make sure the struct file mapping to the
    allocated fd is NULL. Remove this sanity check since it can be assured by
    exisitng zero initilization and NULL set when recycling fd. Meanwhile, add
    likely/unlikely and expand_file() call avoidance to reduce the work under
    file_lock.
    
    Reviewed-by: Tim Chen <tim.c.chen@linux.intel.com>
    Signed-off-by: Yu Ma <yu.ma@intel.com>
    Link: https://lore.kernel.org/r/20240703143311.2184454-2-yu.ma@intel.com
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- fs/file.c
+++ fs/file.c
@@ -515,27 +515,28 @@ repeat:
 	if (fd < files->next_fd)
 		fd = files->next_fd;
 
-	if (fd < fdt->max_fds)
+	if (likely(fd < fdt->max_fds))
 		fd = find_next_fd(fdt, fd);
+
+	error = -EMFILE;
+	if (unlikely(fd >= fdt->max_fds)) {
+		error = expand_files(files, fd);
+		if (error < 0)
+			goto out;
+		/*
+		 * If we needed to expand the fs array we
+		 * might have blocked - try again.
+		 */
+		if (error)
+			goto repeat;
+	}
 
 	/*
 	 * N.B. For clone tasks sharing a files structure, this test
 	 * will limit the total number of files that can be opened.
 	 */
-	error = -EMFILE;
-	if (fd >= end)
+	if (unlikely(fd >= end))
 		goto out;
-
-	error = expand_files(files, fd);
-	if (error < 0)
-		goto out;
-
-	/*
-	 * If we needed to expand the fs array we
-	 * might have blocked - try again.
-	 */
-	if (error)
-		goto repeat;
 
 	if (start <= files->next_fd)
 		files->next_fd = fd + 1;
@@ -546,13 +547,6 @@ repeat:
 	else
 		__clear_close_on_exec(fd, fdt);
 	error = fd;
-#if 1
-	/* Sanity check */
-	if (rcu_access_pointer(fdt->fd[fd]) != NULL) {
-		printk(KERN_WARNING "alloc_fd: slot %d not NULL!\n", fd);
-		rcu_assign_pointer(fdt->fd[fd], NULL);
-	}
-#endif
 
 out:
 	spin_unlock(&files->file_lock);
@@ -618,7 +612,7 @@ void fd_install(unsigned int fd, struct file *file)
 		rcu_read_unlock_sched();
 		spin_lock(&files->file_lock);
 		fdt = files_fdtable(files);
-		BUG_ON(fdt->fd[fd] != NULL);
+		WARN_ON(fdt->fd[fd] != NULL);
 		rcu_assign_pointer(fdt->fd[fd], file);
 		spin_unlock(&files->file_lock);
 		return;
