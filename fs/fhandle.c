49f9daef15ae55fa596b0e1938522e100a9842af
Christian Brauner <brauner@kernel.org>
2024-11-29 13:04:28 +0100

    fhandle: pull CAP_DAC_READ_SEARCH check into may_decode_fh()
    There's no point in keeping it outside of that helper. This way we have
    all the permission pieces in one place.
    
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- fs/fhandle.c
+++ fs/fhandle.c
@@ -287,6 +287,9 @@ static inline bool may_decode_fh(struct handle_to_path_ctx *ctx,
 {
 	struct path *root = &ctx->root;
 
+	if (capable(CAP_DAC_READ_SEARCH))
+		return true;
+
 	/*
 	 * Restrict to O_DIRECTORY to provide a deterministic API that avoids a
 	 * confusing api in the face of disconnected non-dir dentries.
@@ -326,7 +329,7 @@ static int handle_to_path(int mountdirfd, struct file_handle __user *ufh,
 	if (retval)
 		goto out_err;
 
-	if (!capable(CAP_DAC_READ_SEARCH) && !may_decode_fh(&ctx, o_flags)) {
+	if (!may_decode_fh(&ctx, o_flags)) {
 		retval = -EPERM;
 		goto out_path;
 	}
