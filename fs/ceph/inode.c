e560df8b40a1b33e7dcbe52b58028c8e81448b0d
Christian Brauner <brauner@kernel.org>
2026-02-13 12:04:17 +0100

    ceph: fix posix_acl_chmod()
    A long time ago a build failure in cephfs was fixed. As part of that
    change attr->ia_mode was passed to posix_acl_chmod() when really it
    should be inode->i_mode. Fix it.
    
    Fixes: 4db658ea0ca2 ("ceph: Fix up after semantic merge conflict")
    Cc: <stable@kernel.org>
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- fs/ceph/inode.c
+++ fs/ceph/inode.c
@@ -2910,7 +2910,7 @@ int ceph_setattr(struct mnt_idmap *idmap, struct dentry *dentry,
 	err = __ceph_setattr(idmap, inode, attr, NULL);
 
 	if (err >= 0 && (attr->ia_valid & ATTR_MODE))
-		err = posix_acl_chmod(idmap, dentry, attr->ia_mode);
+		err = posix_acl_chmod(idmap, dentry, inode->i_mode);
 
 	return err;
 }
