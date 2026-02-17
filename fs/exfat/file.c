e5f3ba2ba303777b6899657004a621f85f82da05
Chuck Lever <chuck.lever@oracle.com>
2026-02-17 16:47:28 -0500

    exfat: Implement fileattr_get for case sensitivity
    Report exFAT's case sensitivity behavior via the FS_XFLAG_CASEFOLD
    flag. exFAT is always case-insensitive (using an upcase table for
    comparison) and always preserves case at rest.
    
    Acked-by: Namjae Jeon <linkinjeon@kernel.org>
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Link: https://patch.msgid.link/20260217214741.1928576-5-cel@kernel.org
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- fs/exfat/file.c
+++ fs/exfat/file.c
@@ -13,6 +13,7 @@
 #include <linux/msdos_fs.h>
 #include <linux/writeback.h>
 #include <linux/filelock.h>
+#include <linux/fileattr.h>
 
 #include "exfat_raw.h"
 #include "exfat_fs.h"
@@ -279,6 +280,16 @@ int exfat_getattr(struct mnt_idmap *idmap, const struct path *path,
 	stat->btime.tv_sec = ei->i_crtime.tv_sec;
 	stat->btime.tv_nsec = ei->i_crtime.tv_nsec;
 	stat->blksize = EXFAT_SB(inode->i_sb)->cluster_size;
+	return 0;
+}
+
+int exfat_fileattr_get(struct dentry *dentry, struct file_kattr *fa)
+{
+	/*
+	 * exFAT is always case-insensitive (using upcase table).
+	 * Case is preserved at rest (the default).
+	 */
+	fa->fsx_xflags |= FS_XFLAG_CASEFOLD;
 	return 0;
 }
 
@@ -775,6 +786,7 @@ const struct file_operations exfat_file_operations = {
 };
 
 const struct inode_operations exfat_file_inode_operations = {
-	.setattr     = exfat_setattr,
-	.getattr     = exfat_getattr,
+	.setattr	= exfat_setattr,
+	.getattr	= exfat_getattr,
+	.fileattr_get	= exfat_fileattr_get,
 };
