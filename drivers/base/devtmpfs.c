2ee88af48511c1d7105047b1a28ee3b6a18647cc
Christian Brauner <brauner@kernel.org>
2026-03-04 13:06:58 +0100

    devtmpfs: create private mount namespace
    Kernel threads are located in a completely isolated nullfs mount.
    Make it possible for a kthread to create a private mount namespace so it
    can mount private filesystem instances. This is only used by devtmpfs.
    
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- drivers/base/devtmpfs.c
+++ drivers/base/devtmpfs.c
@@ -413,7 +413,7 @@ static noinline int __init devtmpfs_setup(void *p)
 {
 	int err;
 
-	err = ksys_unshare(CLONE_NEWNS);
+	err = kthread_mntns();
 	if (err)
 		goto out;
 	err = init_mount("devtmpfs", "/", "devtmpfs", DEVTMPFS_MFLAGS, NULL);
