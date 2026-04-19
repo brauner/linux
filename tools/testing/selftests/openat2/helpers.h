89f1e153b6fa4a7a1e2f3d5a4ede7d9c3108c099
Jori Koolstra <jkoolstra@xs4all.nl>
2026-04-19 15:21:43 +0200

    selftest: add tests for O_EMPTYPATH
    Add tests for the new O_EMPTYPATH flag of openat(2)/openat2(2).
    
    Also, the current openat2 tests include a helper header file that
    defines the necessary structs and constants to use openat2(2), such as
    struct open_how. This may result in conflicting definitions when the
    system header openat2.h is present as well.
    
    So add openat2.h generated by 'make headers' to the uapi header
    files in ./tools/include and remove the helper file definitions of
    the current openat2 selftests.
    
    Signed-off-by: Jori Koolstra <jkoolstra@xs4all.nl>
    Link: https://patch.msgid.link/20260419132202.487612-1-jkoolstra@xs4all.nl
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- tools/testing/selftests/openat2/helpers.h
+++ tools/testing/selftests/openat2/helpers.h
@@ -14,6 +14,9 @@
 #include <linux/types.h>
 #include "kselftest.h"
 
+#define OPEN_HOW_SIZE_VER0	24
+#define OPEN_HOW_SIZE_LATEST	OPEN_HOW_SIZE_VER0
+
 #define ARRAY_LEN(X) (sizeof (X) / sizeof (*(X)))
 #define BUILD_BUG_ON(e) ((void)(sizeof(struct { int:(-!!(e)); })))
 
@@ -24,44 +27,7 @@
 #define SYS_openat2 __NR_openat2
 #endif /* SYS_openat2 */
 
-/*
- * Arguments for how openat2(2) should open the target path. If @resolve is
- * zero, then openat2(2) operates very similarly to openat(2).
- *
- * However, unlike openat(2), unknown bits in @flags result in -EINVAL rather
- * than being silently ignored. @mode must be zero unless one of {O_CREAT,
- * O_TMPFILE} are set.
- *
- * @flags: O_* flags.
- * @mode: O_CREAT/O_TMPFILE file mode.
- * @resolve: RESOLVE_* flags.
- */
-struct open_how {
-	__u64 flags;
-	__u64 mode;
-	__u64 resolve;
-};
-
-#define OPEN_HOW_SIZE_VER0	24 /* sizeof first published struct */
-#define OPEN_HOW_SIZE_LATEST	OPEN_HOW_SIZE_VER0
-
 bool needs_openat2(const struct open_how *how);
-
-#ifndef RESOLVE_IN_ROOT
-/* how->resolve flags for openat2(2). */
-#define RESOLVE_NO_XDEV		0x01 /* Block mount-point crossings
-					(includes bind-mounts). */
-#define RESOLVE_NO_MAGICLINKS	0x02 /* Block traversal through procfs-style
-					"magic-links". */
-#define RESOLVE_NO_SYMLINKS	0x04 /* Block traversal through all symlinks
-					(implies OEXT_NO_MAGICLINKS) */
-#define RESOLVE_BENEATH		0x08 /* Block "lexical" trickery like
-					"..", symlinks, and absolute
-					paths which escape the dirfd. */
-#define RESOLVE_IN_ROOT		0x10 /* Make all jumps to "/" and ".."
-					be scoped inside the dirfd
-					(similar to chroot(2)). */
-#endif /* RESOLVE_IN_ROOT */
 
 #define E_func(func, ...)						      \
 	do {								      \
