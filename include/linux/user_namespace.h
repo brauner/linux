4879a907335976d27f15c0aa3f1d358db3e9bd9f
Christian Brauner <brauner@kernel.org>
2026-09-11 14:58:40 +0200

    userns: declare the KUnit-visible helpers whenever KUnit is enabled
    VISIBLE_IF_KUNIT drops the static qualifier whenever CONFIG_KUNIT is
    enabled but the prototypes for the two helpers it exposes are only
    declared when CONFIG_USER_NS_MAP_KUNIT_TEST is set.
    
      kernel/user_namespace.c:787:22: warning: no previous prototype for 'insert_extent' [-Wmissing-prototypes]
      kernel/user_namespace.c:860:22: warning: no previous prototype for 'sort_idmaps' [-Wmissing-prototypes]
    
    Guard the declarations on CONFIG_KUNIT so they match the definitions.
    
    Fixes: f6f1a2496ba5 ("userns: Add KUnit test suite for uid_gid_map")
    Reported-by: kernel test robot <lkp@intel.com>
    Closes: https://lore.kernel.org/oe-kbuild-all/202609111048.yhHILE11-lkp@intel.com/
    Closes: https://lore.kernel.org/oe-kbuild-all/202609111305.ta3EOA2x-lkp@intel.com/
    Link: https://patch.msgid.link/20260911-zyklisch-stuhl-zweit-d61b18a17838@brauner
    Signed-off-by: Christian Brauner (Amutable) <brauner@kernel.org>

--- include/linux/user_namespace.h
+++ include/linux/user_namespace.h
@@ -208,11 +208,11 @@ extern bool in_userns(const struct user_namespace *ancestor,
 extern bool current_in_userns(const struct user_namespace *target_ns);
 struct ns_common *ns_get_owner(struct ns_common *ns);
 
-#if IS_ENABLED(CONFIG_USER_NS_MAP_KUNIT_TEST)
+#if IS_ENABLED(CONFIG_KUNIT)
 extern int uid_gid_map_insert_extent(struct uid_gid_map *map,
 				     struct uid_gid_extent *extent);
 extern int uid_gid_map_sort(struct uid_gid_map *map);
-#endif /* CONFIG_USER_NS_MAP_KUNIT_TEST */
+#endif /* CONFIG_KUNIT */
 
 #else
 
